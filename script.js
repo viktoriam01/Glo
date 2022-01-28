c180857d8ccee8613279e31aa234acf9a247af5c
Viktoria <aislingdar@gmail.com>
2022-01-28 17:35:32 +0800

    add homework 2

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-'use strict';
+// 'use strict';
 
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = 'Project';
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 500;
+let rollback = 20;
+let fullPrice = 100000;
+let adaptive = true;
 
-alert('Hello')
-console.log('Hello, world!');
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+
+console.log(`Стоимость верстки экранов ${screenPrice} рублей\nСтоимость разработки сайта ${fullPrice} рублей`);
+
+console.log(screens.toLowerCase().split(", "));
+
+console.log(fullPrice * (rollback/100));
