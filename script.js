3b98afecc5123892b3d18a786330b9faea70e36e
Viktoria Motoshkina <aislingdar@gmail.com>
2022-02-02 12:44:48 +0800

    feat; finished lesson3

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 'use strict';
 
-// let title = prompt("Как называется ваш проект?");
-// let screens = prompt("Какие типы экранов нужно разработать?");
+let title = prompt("Как называется ваш проект?");
+let screens = prompt("Какие типы экранов нужно разработать?");
 let screenPrice = prompt("Сколько будет стоить данная работа?");;
-let rollback = prompt("Сколько будет откат?");
-// let adaptive = prompt("Нужен ли адаптив на сайте?");
+let rollback = 20;
+let adaptive = confirm("Нужен ли адаптив на сайте?");
 
-// let service1 = prompt("Какой дополнительный тип услуги нужен?");
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice1 = prompt("Сколько это будет стоить?");
-// let service2 = prompt("Какой дополнительный тип услуги нужен?");
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice2 = prompt("Сколько это будет стоить?");
 
 let fullPrice = +screenPrice + +servicePrice1 + +servicePrice2;
@@ -17,6 +17,12 @@ console.log(fullPrice)
 let servicePercentPrice = Math.ceil(fullPrice - +rollback);
 console.log(servicePercentPrice);
 
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+
+console.log(`Стоимость верстки экранов ${screenPrice} рублей\nСтоимость разработки сайта ${fullPrice} рублей`);
 
 function showDiscount() {
    if (fullPrice >= 30000) {
@@ -32,5 +38,6 @@ function showDiscount() {
 
 showDiscount()
 
-alert('Hello')
-console.log('Hello, world!');
+console.log(screens.toLowerCase().split(", "));
+
+console.log(fullPrice * (rollback/100));
