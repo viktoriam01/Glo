e0e45544c99f072ba1847a57036db3ef6d957dbf
Viktoria <aislingdar@gmail.com>
2022-02-02 21:46:56 +0800

    feat: merge with lesson3

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 // 'use strict';
 
-<<<<<<< HEAD
-let title = 'Project';
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 500;
-let rollback = 20;
-let fullPrice = 100000;
-let adaptive = true;
-
-
-=======
 let title = prompt("Как называется ваш проект?");
 let screens = prompt("Какие типы экранов нужно разработать?");
 let screenPrice = +prompt("Сколько будет стоить данная работа?");
@@ -41,19 +31,10 @@ showDiscount()
 
 console.log(fullPrice)
 console.log(servicePercentPrice);
->>>>>>> lesson_3
 console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
 console.log(screens.length);
-<<<<<<< HEAD
-
-console.log(`Стоимость верстки экранов ${screenPrice} рублей\nСтоимость разработки сайта ${fullPrice} рублей`);
-
-console.log(screens.toLowerCase().split(", "));
-
-=======
 console.log(`Стоимость верстки экранов ${screenPrice} рублей\nСтоимость разработки сайта ${fullPrice} рублей`);
 console.log(screens.toLowerCase().split(", "));
->>>>>>> lesson_3
 console.log(fullPrice * (rollback/100));
